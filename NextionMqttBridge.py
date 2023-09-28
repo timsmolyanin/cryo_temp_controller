bd1b635608e6a7f78a6642ebfd4476107dd58a56
Smolyanin Timofey <tim.smolyanin@gmail.com>
2023-09-28 11:00:04 +0300

    added docs

--- NextionMqttBridge.py
+++ NextionMqttBridge.py
@@ -9,6 +9,8 @@ import struct
 import list_of_mqtt_topics
 
 
+# The `NextionMqttBridge` class is a thread that connects to a Nextion display via serial
+# communication and bridges it with an MQTT broker.
 class NextionMqttBridge(Thread):
     def __init__(self, parent=None):
         super(NextionMqttBridge, self).__init__(parent)
@@ -23,10 +25,27 @@ class NextionMqttBridge(Thread):
         self.comport_is_open, self.serial_port = params[0], params[1]
 
     def run(self):
+        """
+        The function `run` reads data from a serial port and calls a callback function.
+        """
         self.serial_read(self.comport_is_open, self.serial_port, self.nextion_callback)
     
 
     def serial_connect(self, com: str, baud: int) -> list:
+        """
+        The function `serial_connect` attempts to establish a serial connection to a specified COM port
+        with a given baud rate.
+        
+        :param com: The "com" parameter is a string that represents the COM port to which you want to
+        connect. COM ports are typically used for serial communication with devices such as Arduino
+        boards or other serial devices
+        :type com: str
+        :param baud: The "baud" parameter in the "serial_connect" function is used to specify the baud
+        rate for the serial communication. Baud rate refers to the number of signal changes per second
+        in a communication channel. It determines the speed at which data is transmitted over the serial
+        connection
+        :type baud: int
+        """
         print("Connect to COM-port")
         comport_is_open = False
         serial_port_obj = None
@@ -51,6 +70,17 @@ class NextionMqttBridge(Thread):
 
 
     def serial_read(self, port_is_open, serial_port_obj, cb):
+        """
+        The function `serial_read` reads data from a serial port and passes it to a callback function.
+        
+        :param port_is_open: A boolean variable indicating whether the serial port is open or not
+        :param serial_port_obj: The `serial_port_obj` parameter is an object representing the serial
+        port that you are reading from. It is used to read data from the serial port using the
+        `readline()` method
+        :param cb: The parameter "cb" is a callback function that will be called with the decoded data
+        from the serial port. It is used to process the received data in some way, such as displaying it
+        or performing some action based on it
+        """
         while port_is_open:
             if not port_is_open:
                 break
@@ -74,6 +104,19 @@ class NextionMqttBridge(Thread):
 
 
     def serial_write(self, port_is_open, serial_port_obj, cmd):
+        """
+        The function `serial_write` writes a command to a serial port and handles exceptions if any
+        occur.
+        
+        :param port_is_open: The parameter "port_is_open" is a boolean variable that indicates whether
+        the serial port is currently open or not. It is used to check if the port is open before writing
+        data to it
+        :param serial_port_obj: The `serial_port_obj` parameter is an object representing the serial
+        port connection. It is used to write data to the serial port
+        :param cmd: The `cmd` parameter is a string that represents the command to be written to the
+        serial port. It is encoded using the `encode()` method before being written to the serial port
+        object
+        """
         eof = struct.pack('B', 0xff)
         if port_is_open:
             try:
