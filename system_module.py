8e478acc2e7a1ecf6f8b220780f182112c219a8c
Timophey <tim.smolyanin@gmail.com>
2023-12-13 14:46:55 +0300

    фиксы после тестов system_module

--- system_module.py
+++ system_module.py
@@ -1,22 +1,20 @@
-
-
 from threading import Thread
 import time
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import mqtt_module
-import yaml
 import os
 import nmcli
 from loguru import logger
+import time
+import ipaddress
 
 
 from list_of_mqtt_topics import mqtt_topics_system_module
 
 logger.add("debug.log", format="{time} {level} {message}", level="DEBUG")
 
-# The `PIDControl` class is a threaded class that implements a PID controller for a given MQTT broker
-# and port, with setpoint, feedback, and control topics, and specified PID parameters.
+
 class SystemModule(Thread):
     def __init__(self, mqtt_broker:str, mqtt_port:int, mqtt_user: str, mqtt_password:str, parent=None):
         
@@ -40,44 +38,39 @@ class SystemModule(Thread):
         self.mqtt = mqtt_module.Mqtt(mqtt_broker, mqtt_port, mqtt_user, mqtt_password, self.name, self.on_message_config, self.topic_list)
         self.mqtt.start()
         
-        self.wifi_state = 0
-        self.wifi_mode = None
+        self.wifi_state = 1
+        self.wifi_mode = ""
         
-        self.wifi_client_ssid = None
-        self.wifi_client_password = None
+        self.wifi_client_ssid = ""
+        self.wifi_client_password = ""
         
-        self.wifi_hotspot_ssid = None
-        self.wifi_hotspot_password = None
+        self.wifi_hotspot_ssid = ""
+        self.wifi_hotspot_password = ""
         
-        self.eth_mode = None
-        self.eth_ip = None
-        self.eth_mask = None
-        self.eth_gateway = None
+        self.eth_mode = ""
+        self.eth_ip = ""
+        self.eth_mask = ""
+        self.eth_gateway = ""
         
-    def update_file_list(self):
+    def update_file_list(self, value):
         directory_path = "/root/wk/measure_module/Sensors/ConfigFolder"
         file_list = os.listdir(directory_path)
         logger.debug(f"Список файлов в директории:")
-        
+        term = "\r\n"
         result = ""
         for each in file_list:
-            result += each + '\\r\\'
-            logger.debug(f"    {each}")
-        result += 'n'
+            result += f"{each}{term}"
         logger.debug(f"Строка для топика: {result}")
         self.mqtt.publish_topic(self.topic_list["output_update_files_list"], result)
             
-            
-            
-        
     def set_wifi_state(self, value):
         self.wifi_state = int(value)
         if self.wifi_state == 0:
-            self.wifi_adapter_off()
+            # self.wifi_adapter_off()
             return
         if self.wifi_state == 1:
-            self.wifi_adapter_on()
-            pass
+            # self.wifi_adapter_on()
+            return
     
     def wifi_adapter_off(self):
         nmcli.radio.wifi_off()
@@ -134,44 +127,69 @@ class SystemModule(Thread):
                     nmcli.connection.delete(name=conn.name)
                 except Exception as exc:
                     print(exc)
+    
+    def show_wifi_connections(self):
+        for conn in nmcli.connection():
+            if conn.conn_type == 'wifi':
+                try:
+                    print(conn)
+                    print(type(conn))
+                except Exception as exc:
+                    print(exc)
                 
     def eth_static_mode(self):
-        if self.eth_gateway == "None":
-            logger.debug("Выполняется настройка статического IP адреса без шлюза")
-            nmcli.connection.modify("wb-eth0", {
-                "ipv4.addresses": f"{self.eth_ip}/{self.eth_mask}",
-                "ipv4.method": "manual"
-            })
-        else:
-            logger.debug("Выполняется настройка статического IP адреса с указанием шлюза")
-            nmcli.connection.modify("wb-eth0", {
-                "ipv4.addresses": f"{self.eth_ip}/{self.eth_mask}",
-                "ipv4.gateway": f"{self.eth_gateway}",
-                "ipv4.method": "manual"
-            })
+        logger.debug("Выполняется настройка статического IP адреса без шлюза")
+        static_ip = f"{self.eth_ip}/{self.eth_mask}"
+        cidr_str = str(ipaddress.ip_interface(static_ip))
+        nmcli.connection.modify("eth_port", {
+            "ipv4.addresses": cidr_str,
+            "ipv4.method": "manual"
+        })
+        nmcli.connection.down("eth_port")
+        nmcli.connection.up("eth_port")
+        # else:
+        #     logger.debug("Выполняется настройка статического IP адреса с указанием шлюза")
+        #     nmcli.connection.modify("wb-eth1", {
+        #         "ipv4.addresses": f"{self.eth_ip}/{self.eth_mask}",
+        #         "ipv4.gateway": f"{self.eth_gateway}",
+        #         "ipv4.method": "manual"
+        #     })
     
     def eth_dhcp_mode(self):
         logger.debug("Выполняется получение IP адреса от сервера DHCP")
-        nmcli.connection.modify('wb-eth0', {
+        nmcli.connection.modify('eth_port', {
+            "ipv4.address": "",
             "ipv4.method": "auto"
         })
+        nmcli.connection.down("eth_port")
+        nmcli.connection.up("eth_port")
     
     def wifi_client_mode(self):
+        print(self.wifi_client_ssid)
+        print(self.wifi_client_password)
+        self.wifi_adapter_on()
         try:
             logger.debug(f'Подключение к Wi-Fi сети "{self.wifi_client_ssid}", используя пароль "{self.wifi_client_password}"')
             nmcli.device.wifi_connect(ssid=self.wifi_client_ssid, password=self.wifi_client_password)
+            self.show_wifi_connections()
+            nmcli.connection.up(self.wifi_client_ssid)
             logger.debug(f'Успешное подключение')
         except Exception as exception:
             logger.debug("Ошибка при подключении к сети:")
             logger.debug(f"{exception}")
     
     def wifi_hotspot_mode(self):
+        print(self.wifi_hotspot_ssid)
+        print(self.wifi_hotspot_password)
+        self.wifi_adapter_on()
         try:
             logger.debug(f'Создание точки доступа сети "{self.wifi_hotspot_ssid}", c паролем "{self.wifi_hotspot_password}"')
             nmcli.device.wifi_hotspot(ssid=self.wifi_hotspot_ssid, password=self.wifi_hotspot_password)
             nmcli.connection.modify('Hotspot',{
-                'connection.autoconnect' : "yes"
+                'connection.autoconnect' : "yes",
+                "ipv4.method": "shared"
             })
+            # self.show_wifi_connections()
             nmcli.connection.up("Hotspot")
             logger.debug(f'Точка доступа создана')
         except Exception as exception:
@@ -189,27 +207,14 @@ class SystemModule(Thread):
                         logger.debug(f'    {each.replace("ssid=","")}')
             
 
-#system_module = SystemModule("test", 1, "test", "test")
-#Вай фай (клиент)
-# system_module.set_wifi_client_ssid("TP-Link_B99E")
-# system_module.set_wifi_client_password("lhep201-316")
-# system_module.set_wifi_state(0)
-# system_module.set_wifi_state(1)
-# time.sleep(10)
-# system_module.wifi_scan()
-# system_module.set_wifi_mode("client")
-
-#Вай фай (хост)
-# system_module.set_wifi_hotspot_ssid("Wirenboard controller")
-# system_module.set_wifi_hotspot_password("tangompd")
-# system_module.set_wifi_state(0)
-# system_module.set_wifi_state(1)
-# time.sleep(5)
-# system_module.set_wifi_mode("hotspot")
-
-#Получить список файлов из директории
-#system_module.update_file_list()
+def test():
+    broker = "127.0.0.1"
+    # broker = "192.168.44.11"
+    port = 1883
+    test = SystemModule(broker, port, 'abc', 'abc')
+    while True:
+        time.sleep(1)
 
 
-
-
+if __name__ == "__main__":
+    test()
